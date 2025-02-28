14249476e8036a60dce3094dc09a37d2cc22bdf0
KaisHasan <kais.hasan314@gmail.com>
2025-02-28 13:15:47 +0300

    initial structure of students testimonials

--- js/language.js
+++ js/language.js
@@ -34,4 +34,49 @@ document.addEventListener('DOMContentLoaded', function() {
         currentLang = currentLang === 'en' ? 'ar' : 'en';
         updateLanguage(currentLang);
     });
+
+    /* Testimonials */
+    const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector('.carousel-button--right');
+    const prevButton = document.querySelector('.carousel-button--left');
+
+    let currentSlideIndex = 0;
+
+    const updateSlidePosition = () => {
+        const slideWidth = slides[0].getBoundingClientRect().width;
+        track.style.transform = `translateX(-${slideWidth * currentSlideIndex}px)`;
+    };
+
+    // Arrange the slides next to each other
+    const setSlidePositions = () => {
+        slides.forEach((slide, index) => {
+            slide.style.left = `${index * 100}%`;
+        });
+    };
+
+    setSlidePositions();
+    updateSlidePosition();
+
+    nextButton.addEventListener('click', () => {
+        if (currentSlideIndex === slides.length - 1) {
+            currentSlideIndex = 0; // Loop back to the first slide
+        } else {
+            currentSlideIndex++;
+        }
+        updateSlidePosition();
+    });
+
+    prevButton.addEventListener('click', () => {
+        if (currentSlideIndex === 0) {
+            currentSlideIndex = slides.length - 1; // Loop to the last slide
+        } else {
+            currentSlideIndex--;
+        }
+        updateSlidePosition();
+    });
+
+    // Update slide position on window resize
+    window.addEventListener('resize', updateSlidePosition);
+
 });
